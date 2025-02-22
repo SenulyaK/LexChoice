c0c19639ac01d4f2a98009a229b3cd3389295bce
Your Name <anupamaush69@gmail.com>
2025-02-23 01:34:48 +0530

    Added back to top button.

--- indexScript.js
+++ indexScript.js
@@ -138,3 +138,18 @@ function stopAutoPlay() {
 updateSlides();
 startAutoPlay();
 
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+
+backToTopButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
