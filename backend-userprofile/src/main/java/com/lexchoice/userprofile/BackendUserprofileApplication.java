486aaa27e0fc4d0df26d0f973e2531d9787a6b45
Anupama Ushetti <149264400+AnupamaUshetti@users.noreply.github.com>
2025-03-03 00:17:46 +0530

    Initial commit
    This includes the backend of the user profile section of LexChoice app.

---
+++ backend-userprofile/src/main/java/com/lexchoice/userprofile/BackendUserprofileApplication.java
@@ -0,0 +1,13 @@
+package com.lexchoice.userprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUserprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUserprofileApplication.class, args);
+	}
+
+}
---
+++ backend-userprofile/src/test/java/com/lexchoice/userprofile/BackendUserprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexchoice.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUserprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
