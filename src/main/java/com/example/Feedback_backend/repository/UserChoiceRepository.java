8a5a935aad90e7c1f2f717c45ad15c83779e31ef
SenulyaK <senulya.karunatissa@gmail.com>
2025-03-09 23:38:29 +0530

    Completed repository class

--- src/main/java/com/example/Feedback_backend/repository/UserChoiceRepository.java
+++ src/main/java/com/example/Feedback_backend/repository/UserChoiceRepository.java
@@ -1,4 +1,9 @@
 package com.example.Feedback_backend.repository;
 
-public class UserChoiceRepository {
+import com.example.Feedback_backend.model.UserChoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UserChoiceRepository extends JpaRepository<UserChoice, Long> {
+    List<UserChoice> findByUserId(String userId);
 }
