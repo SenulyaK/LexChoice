4e587914153dba2f704bd25c89dc2455468154f7
SenulyaK <senulya.karunatissa@gmail.com>
2025-03-10 00:16:33 +0530

    Includes the completed repository class

---
+++ src/main/java/com/example/Feedback_backend/service/UserChoiceServices.java
@@ -0,0 +1,22 @@
+package com.example.Feedback_backend.service;
+
+
+import com.example.Feedback_backend.model.UserChoice;
+import com.example.Feedback_backend.repository.UserChoiceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserChoiceServices {
+    private UserChoiceRepository userChoiceRepository;
+
+    public void UserChoiceService(UserChoiceRepository userChoiceRepository) {
+        this.userChoiceRepository = userChoiceRepository;
+    }
+
+    public UserChoiceServices(UserChoiceRepository userChoiceRepository) {
+        this.userChoiceRepository = userChoiceRepository;
+    }
+    
+}
