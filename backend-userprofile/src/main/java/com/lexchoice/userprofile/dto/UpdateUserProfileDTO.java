6a2148159a6b843079481cded476a08f90df97b2
Anupama Ushetti <anupamaush69@gmail.com>
2025-03-09 23:30:26 +0530

    Updated the exception class.

--- backend-userprofile/src/main/java/com/lexchoice/userprofile/dto/UpdateUserProfileDTO.java
+++ backend-userprofile/src/main/java/com/lexchoice/userprofile/dto/UpdateUserProfileDTO.java
@@ -1,4 +1,17 @@
 package com.lexchoice.userprofile.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserProfileDTO {
+    private String name;
+    private String email;
+    private String profilePicture;
+    private Boolean notificationEnabled;
+    private String language;
+    private String password; // Optional field
 }
+
--- backend-userprofile/src/main/java/com/lexchoice/userprofile/exception/UserNotFoundException.java
+++ backend-userprofile/src/main/java/com/lexchoice/userprofile/exception/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.lexchoice.userprofile.exception;
 
-public class UserNotFoundException {
-
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
