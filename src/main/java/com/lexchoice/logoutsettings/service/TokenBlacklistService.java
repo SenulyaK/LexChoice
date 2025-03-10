b913326ae33447e23f4ceb0ea68d0af23333bd1b
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 19:03:11 +0530

    Importing for tokenblack list service

---
+++ src/main/java/com/lexchoice/logoutsettings/service/TokenBlacklistService.java
@@ -0,0 +1,8 @@
+package com.lexchoice.logoutsettings.service;
+
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TokenBlacklistService {
+}
