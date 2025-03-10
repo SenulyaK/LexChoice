81df6aa0ce32410cdc43bb9094bc0e00a4790231
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 19:52:00 +0530

    starts the application

---
+++ src/main/java/LexChoiceApplication.java
@@ -0,0 +1,18 @@
+package com.lexchoice.;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@SpringBootApplication
+public class LexChoiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LexChoiceApplication.class, args);
+    }
+}
+
+
+
