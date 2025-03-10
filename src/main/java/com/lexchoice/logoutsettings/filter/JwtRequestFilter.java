59636d28c3e25cd2c06acb624eb5d7ea8a612f33
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 19:12:22 +0530

    importing for Jwt request filter

---
+++ src/main/java/com/lexchoice/logoutsettings/filter/JwtRequestFilter.java
@@ -0,0 +1,17 @@
+package com.lexchoice.logoutsettings.filter;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtRequestFilter {
+}
