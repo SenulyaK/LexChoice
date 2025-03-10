d6248ec87d126925e6caaf230df54e608c3bdc6d
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 19:23:49 +0530

    JWT handling

--- src/main/java/com/lexchoice/logoutsettings/util/JwtUtil.java
+++ src/main/java/com/lexchoice/logoutsettings/util/JwtUtil.java
@@ -8,5 +8,15 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.function.Function;
 
+@Service
 public class JwtUtil {
-}
+    private final String SECRET_KEY = "your_secret_key";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
