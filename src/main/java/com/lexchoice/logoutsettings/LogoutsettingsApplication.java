4a20269a98641183e630eaa90e2f88ffe45d1e40
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 16:10:12 +0530

    1st commit for logout

---
+++ src/main/java/com/lexchoice/logoutsettings/LogoutsettingsApplication.java
@@ -0,0 +1,13 @@
+package com.lexchoice.logoutsettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogoutsettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogoutsettingsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lexchoice/logoutsettings/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.lexchoice.logoutsettings.controller;
+
+public class AuthController {
+}
