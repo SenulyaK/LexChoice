4f0add79573b3caf69f91ff70c9c1f6f72a03281
Ragavi1006 <kragavi1063@gmail.com>
2025-03-10 19:16:07 +0530

    Checks if the token is blacklisted before processing request

--- src/main/java/com/lexchoice/logoutsettings/filter/JwtRequestFilter.java
+++ src/main/java/com/lexchoice/logoutsettings/filter/JwtRequestFilter.java
@@ -13,5 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtRequestFilter {
-}
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final TokenBlacklistService tokenBlacklistService;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    public JwtRequestFilter(TokenBlacklistService tokenBlacklistService, JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.tokenBlacklistService = tokenBlacklistService;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
